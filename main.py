589de7629d3d183a22a32d0083281ef922343fd1
888-coding <pachelee@gmail.com>
2025-07-22 21:01:21 -0300

    Criação da tabela de produtos

--- main.py
+++ main.py
@@ -5,9 +5,20 @@ def create_database():
 	con = sqlite3.connect("database.db")
 	con.close()
 
-def create_table_product():
-	sql = ""
+def create_table_products():
 	con = sqlite3.connect("database.db")
+	sql = """
+	CREATE TABLE products(
+	id INTEGER PRIMARY KEY, 
+	name TEXT, 
+	price INTEGER
+	)
+	"""
 	cur = con.cursor()
-	
+	cur.execute(sql)
+	con.close()
+
+
+
 create_database()
+create_table_products()
