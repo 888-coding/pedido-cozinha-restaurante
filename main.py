52be3b7b3484892386307eb37e359637cf67a0bc
888-coding <pachelee@gmail.com>
2025-07-22 21:17:41 -0300

    Criação da tabela de Pedidos

--- main.py
+++ main.py
@@ -1,24 +1,42 @@
 # main.py 
 import sqlite3
 
+# Criação do banco de dados 
 def create_database():
 	con = sqlite3.connect("database.db")
 	con.close()
 
+# Criação da tabela de produtos
 def create_table_products():
 	con = sqlite3.connect("database.db")
 	sql = """
-	CREATE TABLE products(
-	id INTEGER PRIMARY KEY, 
-	name TEXT, 
-	price INTEGER
-	)
+		CREATE TABLE IF NOT EXISTS products(
+		id INTEGER PRIMARY KEY, 
+		name TEXT, 
+		price INTEGER
+		)
 	"""
 	cur = con.cursor()
 	cur.execute(sql)
 	con.close()
 
+# Criaçao da tabela de pedidos 
+def create_table_orders():
+	con = sqlite3.connect("database.db")
+	sql = """
+		CREATE TABLE IF NOT EXISTS orders(
+		id INTERGER PRIMARY KEY,
+		table_number TEXT, 
+		order_date DATE, 
+		total_value INTEGER 
+		)
+	"""
+	cur = con.cursor()
+	cur.execute(sql)
+	con.close()
 
-
+# Procedimentos
 create_database()
 create_table_products()
+create_table_orders()
+
