3f13cc3ca6372681b05d8d32f03f2eff50815a4e
888-coding <pachelee@gmail.com>
2025-09-04 21:35:22 -0300

    Criacao das tabelas feitas.

--- create_table.py
+++ create_table.py
@@ -13,7 +13,58 @@ def create_tables():
 
     con = sqlite3.connect("database.db")
     cur = con.cursor()
-    sql = "CREATE TABLE IF NOT EXIST orders x x x"
+    sql = """
+        CREATE TABLE IF NOT EXISTS orders (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            table_number TEXT NOT NULL,
+            order_date TEXT NOT NULL,
+            order_time TEXT NOT NULL,
+            total_value INTEGER NOT NULL
+        )
+        """
     cur.execute(sql,())
-    row = cur.fetchall()
     con.close()
+
+    # Criação da tabela:  "products"
+    # Campos : id, name_chinese, name_portuguese, price
+    #
+    #
+
+    con = sqlite3.connect("database.db")
+    cur = con.cursor()
+    sql = """
+            CREATE TABLE IF NOT EXISTS products (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                code TEXT NOT NULL,
+                name_chinese TEXT NOT NULL, 
+                name_portuguese TEXT NOT NULL, 
+                price INTEGER
+            ) 
+    """
+    cur.execute(sql, ())
+    con.close()
+
+
+    # Criação da tabela : order_items
+    # id, order_id
+    # product_id, product_qty, product_price
+    #
+    #
+    #
+
+    con = sqlite3.connect("database.db")
+    cur = con.cursor()
+    sql = """
+        CREATE TABLE IF NOT EXISTS order_items(
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        order_id INTEGER NOT NULL, 
+        product_id INTEGER NOT NULL, 
+        product_qty INTEGER NOT NULL, 
+        product_price INTEGER NOT NULL
+        )
+    """
+    cur.execute(sql, ())
+    con.close()
+
+
+create_tables()
