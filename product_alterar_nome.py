f885723b3820daa929dd414468fe4a8f11415114
888-coding <pachelee@gmail.com>
2025-09-05 00:53:36 -0300

    Criado o arquivo

---
+++ product_alterar_nome.py
@@ -0,0 +1,12 @@
+import os 
+import sqlite3
+
+def alterar():
+    
+    # Tabela : products 
+    # id, code, name_chinese, name_portuguese, price
+    # > id, code, name_chinese, name_portugues
+    #
+    #
+
+alterar()
