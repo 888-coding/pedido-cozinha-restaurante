fdbec59ddd1ee66e52b2fbf583b644b5d4e869d1
888-coding <pachelee@gmail.com>
2025-09-05 00:40:21 -0300

    Input do novo preco feito, e a atualizacao feita

--- produto_alterar_preco.py
+++ produto_alterar_preco.py
@@ -31,14 +31,43 @@ def alterar():
         else:
             break
 
-    print(row)
-    
+    time.sleep(0.5)
+
     id = row[0]
     code = row[1]
     name_chinese = row[2]
     name_portugues = row[3]
     old_price = row[4]
 
-    print(f"Codigo : {code} - {name_chinese}  {name_portugues}  Preco antigo: {old_price}")
+    old_price = float(old_price) / 100 
+
+    print(f"Codigo : {code}")
+    time.sleep(0.4)
+    print(f"Nome : {name_chinese} - {name_portugues}")
+    time.sleep(0.4)
+    print(f"Preço: {old_price:.2f}")
+    time.sleep(0.4)
+
+    while True:
+        deseja = input("Deseja alterar (s/n) ? : ").upper()
+        if deseja == "S" or deseja == "N":
+            break
+    
+    if deseja == "S":
+        # Novo valor 
+        new_price = input("Digite o novo valor : R$ ")
+        new_price = float(new_price) * 100
+
+        # Atualizar no banco de dados 
+        con = sqlite3.connect("database.db")
+        cur = con.cursor()
+        sql = "UPDATE products SET price = ? WHERE id = ? "
+        cur.execute(sql, (new_price, id, ) )
+        con.commit()
+        con.close()
+
+        print("Preço alterado com sucesso ! ")
+    else:
+        print("Ok. Não foi alterado o preço")
 
 alterar()
