1ec37e959a10a81ec81a3231b6f816c166722116
888-coding <pachelee@gmail.com>
2025-09-04 21:50:06 -0300

    input feito

--- produto_cadastro.py
+++ produto_cadastro.py
@@ -5,12 +5,23 @@ from datetime import datetime
 
 def produto_cadastro():
     
-    #
-    #
-    #
-    #
+    # Cadastrar produto 
+    # Tabela : id, code, name_chinese, name_portuguese, price
+    # 
+    # 
 
+    os.system("cls")
+    
+    print("==========================")
+    print(" >  Cadastro de produto ")
+    print("==========================")
 
+    code = input("\n > CODIGO DO PRODUTO : ")
+    name_chinese = input("> NOME CHINES : ")
+    name_portuguese = input("> NOME PORTUGUES : ")
+    input_price = input("> PREÇO : R$ ")
 
+    price = input_price * 100
+    
 
 produto_cadastro()
