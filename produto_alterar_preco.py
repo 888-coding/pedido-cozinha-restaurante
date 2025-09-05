5fe3744d2832b695dc09825bcaf36dfe60da1364
888-coding <pachelee@gmail.com>
2025-09-05 00:19:53 -0300

    Consulta do codigo feita

--- produto_alterar_preco.py
+++ produto_alterar_preco.py
@@ -3,9 +3,42 @@ import sqlite3
 from datetime import datetime
 import time 
 
+def alterar():
+    
+    # Tabela products 
+    # id, code, name_chinese, name_portuguese, price  
+    # 
+    # 
+    # 
 
+    while True:    
+        # input do id para procurar 
+        os.system("cls")
+        print("====================")
+        print("> Procurar produto  ")
+        print("====================")
 
+        code = str(input("\n> Digite o codigo : ").upper())
+        
+        con = sqlite3.connect("database.db")
+        cur = con.cursor()
+        sql = "SELECT id, code, name_chinese, name_portuguese, price FROM products WHERE code = ? "
+        cur.execute(sql, (code,) )
+        row = cur.fetchone()
+        con.close()
+        if not row:
+            input("Nao encontrado!")
+        else:
+            break
 
+    print(row)
+    
+    id = row[0]
+    code = row[1]
+    name_chinese = row[2]
+    name_portugues = row[3]
+    old_price = row[4]
 
+    print(f"Codigo : {code} - {name_chinese}  {name_portugues}  Preco antigo: {old_price}")
 
-
+alterar()
