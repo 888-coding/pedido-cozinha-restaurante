6a7f0552a5cdcc0aad832705c448f5a79f8df8bb
888-coding <pachelee@gmail.com>
2025-09-05 00:59:19 -0300

    Consulta do codigo

--- product_alterar_nome.py
+++ product_alterar_nome.py
@@ -9,4 +9,18 @@ def alterar():
     #
     #
 
+    print("============================")
+    print(" > Alterar nome do produto")
+    print("============================")
+
+    # Consultar o codigo produto
+    code = input("\nDigite o codigo do produto : ")
+    
+    con = sqlite3.connect("database.db")
+    cur = con.cursor()
+    sql = "SELECT id, code, name_chinese, name_portuguese FROM products WHERE code = ? "
+    cur.execute( sql, (code, ) )
+    row = cur.fetchone()
+    con.close()
+
 alterar()
