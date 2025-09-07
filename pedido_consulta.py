58916194fb94bf064a5ceffc445260b72a48b313
888-coding <pachelee@gmail.com>
2025-09-06 22:18:25 -0300

    Criando arquivo para consulta de pedido

---
+++ pedido_consulta.py
@@ -0,0 +1,18 @@
+import os
+import sqlite3
+import time 
+from datetime import datetime
+
+def consultar():
+
+   # Mostrar um Menu de qual opcao deseja consultar
+   # 1- Listar pedido do dia 
+   # 2- Outro periodo 
+   # 
+   # 
+   #  
+
+
+
+
+consultar()
