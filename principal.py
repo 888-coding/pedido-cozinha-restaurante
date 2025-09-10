af370367885b9f7e0626f2036cdf876c82712f2f
888-coding <pachelee@gmail.com>
2025-09-09 22:55:57 -0300

    principal menu feito com saida do sistema

--- principal.py
+++ principal.py
@@ -9,6 +9,29 @@ def principal():
     print(" > Sistema de Pedidos ")
     print("====================================")
 
+    print("1. Pediddos")
+    print("2. Produtos")
+    print("3. Relatórios")
+    print("4. Sair\n\n")
+
+    while True:
+        opcao = input("Opção : ")
+        if opcao == "1" or opcao == "2" or opcao =="3" or opcao == "4":
+            break
+    
+    if opcao == "1":
+        pedidos()
+    elif opcao == "2":
+        produtos()
+    elif opcao == "3":
+        relatorios()
+    else:
+        print("\n\n=================================")
+        print(" > Obrigado !! ")
+        print("=================================")
+        time.sleep(3)
+        os.system("cls")
+        exit()
 
 
-principal
+principal()
