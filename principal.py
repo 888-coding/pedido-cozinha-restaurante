26fc6cb22e674537fa0c43b7379341835d64ca9c
888-coding <pachelee@gmail.com>
2025-09-09 22:37:44 -0300

    Consulta de outros periodos feito.

---
+++ principal.py
@@ -0,0 +1,14 @@
+import os
+import sqlite3
+import time
+from datetime import datetime
+
+def principal():
+    os.system("cls")
+    print("====================================")
+    print(" > Sistema de Pedidos ")
+    print("====================================")
+
+
+
+principal
