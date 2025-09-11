f8dcc7de4f1681c7a7102a36a2d20a0277a22d7c
888-coding <pachelee@gmail.com>
2025-09-11 10:27:45 -0300

    montando tela de menu

---
+++ produtos.py
@@ -0,0 +1,31 @@
+import os
+from datetime import datetime
+import time
+
+def menu_produtos():
+    while True:
+        os.system("cls")
+        print("SISTEMA 7P > MENU DE PRODUTOS")
+        print("1. Cadastro")
+        print("2. Consulta")
+        print("3. Alterar preco")
+        print("4. Alterar nome\n\n")
+        
+        opcao = input("> Opcao: ")
+
+        if opcao == "1":
+            pass
+        elif opcao == "2":
+            pass
+        elif opcao == "3":
+            pass
+        if opcao == "4":
+            pass
+        elif opcao == "0":
+            break
+        else: 
+            print("Opcao invalida")
+
+
+
+
