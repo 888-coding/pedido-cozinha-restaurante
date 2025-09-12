c25ad310f47634fac714fbe7ca8c8ab852c29139
888-coding <pachelee@gmail.com>
2025-09-11 22:47:46 -0300

    adicionado consulta de todos os produtos

--- produtos.py
+++ produtos.py
@@ -18,7 +18,7 @@ def menu_produtos():
         if opcao == "1":
             produto_cadastrar()
         elif opcao == "2":
-            pass
+            produtos_consultar()
         elif opcao == "3":
             alterar_preco()
         if opcao == "4":
@@ -61,6 +61,28 @@ def produto_cadastrar():
 
     print("Cadastrado com sucesso ! ")
     time.sleep(1)
+
+def produtos_consultar():
+    os.system("cls")
+    print("=================================")
+    print(" > Consultar todos os produtos")
+    print("=================================")
+
+    con = sqlite3.connect("database.db")
+    cur = con.cursor()
+    sql = "SELECT id, code, name_chinese, name_portuguese, price FROM products ORDER BY code ASC"
+    cur.execute(sql, ())
+    rows = cur.fetchall()
+    cur.close()
+    con.close()
+
+    for row in rows:
+        print(f"Codigo : {row[1]} - {row[2]} {row[3]} Preço: {float(row[4])/100:.2f}")
+
+    print("\n\n Fim de linha .")
+    input("Tecle para continuar ..")
+    time.sleep(1.5)   
+
 
 def alterar_preco():
     
