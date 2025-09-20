591b4b0569ac88580b1f9d4ff8750d6e9be5c2cd
888-coding <pachelee@gmail.com>
2025-09-20 08:53:34 -0300

    Relatorio de vendas por dia

--- relatorios.py
+++ relatorios.py
@@ -1,5 +1,6 @@
 import os
-
+import sqlite3
+from datetime import date
 
 def menu_relatorios():
     while True:
@@ -10,7 +11,7 @@ def menu_relatorios():
         print("1. Relatorio de vendas por dia")
         print("2. Relatorio de vendas por mês")
         print("3. Relatorio de vendas por periodo")
-        print("0. Sair")
+        print("0. Voltar")
 
         print("===============================")
         opcao = input(" > Digite a opcao : ")
@@ -21,13 +22,31 @@ def menu_relatorios():
 
 def relatorio_vendas_dia():
     
-
     os.system("cls")
     print("=================================")
     print(" > Relatório vendas por dia ")
     print("=================================")
     
-    input("\n\n > Digite a data (aaaa-mm-dd): ")
+    data = input("\n\n > Digite a data (aaaa-mm-dd): ")
+
+    con = sqlite3.connect("database.db")
+    cur = con.cursor()
+    sql = "SELECT * FROM orders WHERE order_date = ?"
+    cur.execute(sql, (data,))
+    rows = cur.fetchall()
+    cur.close()
+    con.close()
+    if rows:
+        for row in rows:
+            print(row[0])
+            pass
+        pass
+    else:
+        os.system("cls")
+        print("Não encontrado nenhum pedido na data.")
+        input("Digite algo para continuar ...")
+
+    
 
 
     
