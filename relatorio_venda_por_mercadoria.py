bae90e68fa32bced5a02bf92520a1a62717fe1fe
888-coding <pachelee@gmail.com>
2025-09-25 22:27:24 -0300

    Criando inicio de relatorio

---
+++ relatorio_venda_por_mercadoria.py
@@ -0,0 +1,13 @@
+import os 
+def relatorio_venda_por_mercadorias():
+    os.system("cls")
+    print("======================================")
+    print(" > Relatorio Vendas por mercadorias")
+    print("======================================")
+
+    print("\nPeriodo Mensal ")
+    ano = input("Digite ano: ")
+    mes = input("Digite o mes: ")
+
+
+relatorio_venda_por_mercadorias()
