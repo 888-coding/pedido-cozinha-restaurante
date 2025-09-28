79dcb169ac239e15f06d4784fa8432171fc96af1
888-coding <pachelee@gmail.com>
2025-09-27 22:55:36 -0300

    Montado o quantidade total sem periodo

--- relatorio_venda_por_mercadoria.py
+++ relatorio_venda_por_mercadoria.py
@@ -9,6 +9,7 @@ def relatorio_venda_por_mercadorias():
     print("\nPeriodo Mensal ")
     ano = input("Digite ano: ")
     mes = input("Digite o mes: ")
+    print("\n\n")
 
     con = sqlite3.connect("database.db")
     cur = con.cursor()
@@ -24,7 +25,20 @@ def relatorio_venda_por_mercadorias():
     
     if rows:
         for row in rows:
-            print(row)
+            # os id de produtos
+            id_product = row[0]
+            con = sqlite3.connect("database.db")
+            cur = con.cursor()
+            sql =   """
+                    SELECT sum(product_qty) AS soma FROM order_items 
+                    WHERE product_id = ?
+                    """
+            cur.execute(sql,(id_product,))
+            rows = cur.fetchone()
+            cur.close()
+            con.close()
+            print("Quantidade por produto")
+            print(f"> id do produto : {id_product} .  Quantidade : {rows[0]}\n")
     else:
         print("Não foi encontrado nenhum registro.")
 
