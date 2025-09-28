ff38d1578319de1badee894c4b2037952a035bfa
888-coding <pachelee@gmail.com>
2025-09-27 22:37:28 -0300

    Sql de relatorio preparatorio

--- relatorio_venda_por_mercadoria.py
+++ relatorio_venda_por_mercadoria.py
@@ -1,4 +1,5 @@
 import os 
+import sqlite3
 def relatorio_venda_por_mercadorias():
     os.system("cls")
     print("======================================")
@@ -9,5 +10,22 @@ def relatorio_venda_por_mercadorias():
     ano = input("Digite ano: ")
     mes = input("Digite o mes: ")
 
+    con = sqlite3.connect("database.db")
+    cur = con.cursor()
+    sql =   """
+            SELECT DISTINCT oi.product_id FROM orders AS o
+            JOIN order_items AS oi 
+            ON o.id = oi.order_id
+            """
+    cur.execute(sql,() )
+    rows = cur.fetchall()
+    cur.close()
+    con.close()
+    
+    if rows:
+        for row in rows:
+            print(row)
+    else:
+        print("Não foi encontrado nenhum registro.")
 
 relatorio_venda_por_mercadorias()
