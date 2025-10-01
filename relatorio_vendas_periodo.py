14fb6c6bf1f233e26a61e9d49f38bf8e6ffa87aa
888-coding <pachelee@gmail.com>
2025-10-01 17:50:58 -0300

    Adicionado o total de vendas do periodo

--- relatorio_vendas_periodo.py
+++ relatorio_vendas_periodo.py
@@ -1,5 +1,7 @@
 import os 
 import sqlite3
+from rich.console import Console
+from rich.table import Table
 
 def relatorio_vendas_periodo():
     os.system("cls")
@@ -23,13 +25,37 @@ def relatorio_vendas_periodo():
 
     con = sqlite3.connect("database.db")
     cur = con.cursor()
-    sql = "SELECT * FROM orders WHERE order_date >= ? AND order_date <= ? "
+    sql = "SELECT id, order_date, order_time, total_value FROM orders WHERE order_date >= ? AND order_date <= ? "
     cur.execute(sql, (inicio_data, fim_data, ) )
     rows = cur.fetchall()
     cur.close()
     con.close()
 
+    console = Console()
+    tabela = Table(title="Relatório de vendas por periodo")  
+    tabela.add_column("No Pedido")
+    tabela.add_column("Data")
+    tabela.add_column("Horario", justify="right")
+    tabela.add_column("Valor do pedido", justify="right", style="green")
 
-    for row in rows:
-        print(row)
+    if rows:
+        # Existe dados 
+        valor_total_periodo = 0 
+        for row in rows:
+            valor_total_periodo = valor_total_periodo + row[3]
+            
+            pedido_id = row[0]
+            pedido_data = row[1]
+            pedido_horario = row[2]
+            pedido_valor = float(row[3])/100
+            
+            valor_formatado = f"{pedido_valor:.2f}"
+            
+            tabela.add_row(str(pedido_id), pedido_data, pedido_horario, str(valor_formatado))
+        valor_formatado = f"{float(valor_total_periodo)/100:.2f}"
+        tabela.add_row(" ", " ", "Total: ", str(valor_formatado))
+    else:
+        # Não existe dados 
+        tabela.add_row("n/d","n/d", "n/d", "n/d")
+    console.print(tabela)
 relatorio_vendas_periodo()
