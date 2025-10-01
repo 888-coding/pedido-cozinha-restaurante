96089086c20c527a201a8b90eca959bdd0e39b58
888-coding <pachelee@gmail.com>
2025-10-01 14:32:27 -0300

    Teste de tabela com Rich

--- teste_rich.py
+++ teste_rich.py
@@ -1,5 +1,5 @@
 from rich.console import Console
-from rich.table import Table 
+from rich.table import Table
 
 console = Console()
 
@@ -7,10 +7,11 @@ tabela = Table(title="Exempo de Tabela com Rich")
 
 tabela.add_column("Nome", style="cyan", no_wrap=True)
 tabela.add_column("Idade", justify="right", style="magenta")
-tabela.add_column("País", justify="right",style="green")
+tabela.add_column("País", justify="right", style="green")
 
 tabela.add_row("João", "25", "Brasil")
 tabela.add_row("João", "25", "Brasil")
 tabela.add_row("João", "25", "Brasil")
 
 console.print(tabela)
+
