d60c127d6f3e8e31932505348d4708e14c455295
888-coding <pachelee@gmail.com>
2025-10-01 21:31:45 -0300

    tabela com rich

--- produtos_consultar.py
+++ produtos_consultar.py
@@ -1,6 +1,9 @@
 import os 
 import sqlite3
 import time
+from rich.console import Console
+from rich.table import Table
+
 
 def produtos_consultar():
     os.system("cls")
@@ -8,6 +11,9 @@ def produtos_consultar():
     print(" > Consultar todos os produtos")
     print("=================================")
 
+
+    console = Console()
+    tabela = Table(title="Produtos ")
     con = sqlite3.connect("database.db")
     cur = con.cursor()
     sql = "SELECT id, code, name_chinese, name_portuguese, price FROM products ORDER BY code ASC"
@@ -16,9 +22,17 @@ def produtos_consultar():
     cur.close()
     con.close()
 
-    for row in rows:
-        print(f"Codigo : {row[1]} - {row[2]} {row[3]} Preço: {float(row[4])/100:.2f}")
+    tabela.add_column("CODIGO", style="cyan")
+    tabela.add_column("DESCRICAO", style="white")
+    tabela.add_column("PRECO", style="green")
 
+    for row in rows:
+        valor_formatado = str(f"{float(row[4])/100:.2f}")
+        code = str(row[1])
+        descricao = row[2] + " - " + row[3]
+        tabela.add_row(code, descricao, valor_formatado)
+
+    console.print(tabela)
     print("\n\n Fim de linha .")
     input("Tecle para continuar ..")
     time.sleep(1.5)
