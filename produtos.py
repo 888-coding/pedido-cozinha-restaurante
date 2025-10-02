9ee43fe732178ab7ea40cde5e7441f56b14b6935
888-coding <pachelee@gmail.com>
2025-10-01 21:53:38 -0300

    Retornar funcao de cadastro que sem querer estava deletado

--- produtos.py
+++ produtos.py
@@ -34,6 +34,39 @@ def menu_produtos():
             print("Opcao invalida.")
             time.sleep(0.5)
 
+def produto_cadastrar():
+    
+    # Cadastrar produto 
+    # Tabela : id, code, name_chinese, name_portuguese, price
+    # 
+    # 
+
+    os.system("cls")
+    
+    print("==========================")
+    print(" >  Cadastro de produto ")
+    print("==========================")
+
+    code = input("\n> CODIGO DO PRODUTO : ").upper()
+    name_chinese = input("> NOME CHINES : ")
+    name_portuguese = input("> NOME PORTUGUES : ").upper()
+    input_price = input("> PREÇO : R$ ")
+
+    price = float(input_price) * 100
+    
+    # Inserir dados , code, name_chinese, name_portuguese, price
+    con = sqlite3.connect("database.db")
+    cur = con.cursor()
+    sql = """
+        INSERT INTO products (code, name_chinese, name_portuguese, price)
+        VALUES (?, ?, ?, ?)
+    """
+    cur.execute(sql, (code, name_chinese, name_portuguese, price) )
+    con.commit()
+    con.close()
+
+    print("Cadastrado com sucesso ! ")
+
 
 def produtos_consultar():
     os.system("cls")
