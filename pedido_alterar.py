3a9dfa7871557c6c56085c95df2fc3fc0c949d3b
888-coding <pachelee@gmail.com>
2025-10-05 00:53:43 -0300

    Cadastrando o menu

--- pedido_alterar.py
+++ pedido_alterar.py
@@ -1,3 +1,17 @@
 import os 
 import sqlite3
+from rich.console import Console
+from rich.table import Table
 
+def pedido_alterar():
+    os.system("cls")
+
+    print("==================================")
+    print("Alterar pedido ")
+    print("==================================")
+
+    input("> Digite o numero do pedido : ")
+
+
+
+pedido_alterar()
