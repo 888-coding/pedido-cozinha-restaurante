6b5a8717518bb9a91fdbc4538a9d784c37c6f72e
888-coding <pachelee@gmail.com>
2025-10-30 23:10:53 -0300

    Alterar tabela de pedidos com status e valor recebido

--- script_alter_table.py
+++ script_alter_table.py
@@ -3,6 +3,28 @@
 import sqlite3
 
 def alterar_tabela_pedidos():
-    pass
+    # Acrescentar 2 campos :
+    # 1. Status 
+    # 2. Valor recebido 
 
+    # 1. Status: Precisa dizer se o pedido foi iniciado, concluido, cancelado.
+    # 2. Valor recebido, porque tem descontos.
+       
+    con = sqlite3.connect("database.db")
+    cur = con.cursor()
+    sql = "ALTER TABLE orders ADD COLUMN order_status TEXT;"
+    cur.execute(sql)
+    con.commit()
+    
+    print("\n> Adicionado coluna status a tabela de Pedidos.")
+
+    sql = "ALTER TABLE orders ADD COLUMN received_value INT;"
+    cur.execute(sql)
+    con.commit()
+
+    print("\n> Adicionado coluna de valor recebido a tabela de Pedidos.")
+    cur.close()
+    con.close()
+
+alterar_tabela_pedidos()
 
